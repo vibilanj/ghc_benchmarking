6821c10968545c5ed2653ea1184f3a3b62849ea9
Vibilan Jayanth <vibilan@firstindo.com>
2024-07-15 13:35:54 +0800

    add docstrings and typing to timing_analysis and plot_generation

--- plot_generation.py
+++ plot_generation.py
@@ -1,28 +1,73 @@
+"""
+This file contains the functions that deal with plotting the timing data. The
+functions in this file generate scatter plots comparing the recorded statistics
+at the module and package level.
+"""
+
 import os
-import pandas as pd
+from typing import List, Optional
 import matplotlib.pyplot as plt
 import numpy as np
+import pandas as pd
 
-from constants import (
-    PLOTS_DIR, MODULE_STATS_FILE, PACKAGE_STATS_FILE,
-    CB_COLORS, PLOT_STYLES, DEFAULT_PLOT_STYLE, PACKAGE_STYLE
-)
+from constants import (CB_COLORS, DEFAULT_PLOT_STYLE, MODULE_STATS_FILE,
+                       PACKAGE_STATS_FILE, PACKAGE_STYLE, PLOT_STYLES,
+                       PLOTS_DIR)
 
 
-def plot_metric_vs_metric(df, x, y, xlabel, ylabel, title, filename,
-                          guide_pcts, guide_label, log = True, module = True):
+# TODO: prefer default arguments or not?
+def plot_metric_vs_metric(
+    df: pd.DataFrame,
+    x: str,
+    y: str,
+    xlabel: str,
+    ylabel: str,
+    title: str,
+    filename: str,
+    guide_pcts: List[int],
+    guide_label: str,
+    log: Optional[bool] = True,
+    module: Optional[bool] = True
+) -> None:
+    """
+    Plots a metric against another metric. The plot is saved as a PDF in the
+    `plots` directory.
+
+    Args:
+        df: The dataframe containing the data to plot
+        x: The column to plot on the x-axis
+        y: The column to plot on the y-axis
+        xlabel: The label for the x-axis
+        ylabel: The label for the y-axis
+        title: The title of the plot
+        filename: The name of the file to save the plot as
+        guide_pcts: The percentages to plot guide lines for
+        guide_label: The label for the guide lines
+        log: Whether to use a log scale for the axes
+        module: Whether to plot by module or package
+
+    Returns:
+        None
+    """
+
     fig, ax = plt.subplots()
 
     if module:
+        # For module plot, group by extension and plot each group with a different style
         for ext, ext_df in df.groupby("extension"):
             style = PLOT_STYLES.get(ext, DEFAULT_PLOT_STYLE)
             ax.scatter(ext_df[x], ext_df[y], alpha = 0.5, label = ext, **style)
     else:
+        # For package plot, plot all packages with the same style
         ax.scatter(df[x], df[y], **PACKAGE_STYLE)
 
-    if log is True:
+    if log:
+        # Setting log scale for both axes
         ax.set(xscale = "log", yscale = "log")
+        # Changing filename to indicate log scale
+        filename = filename.replace(".", "_log.")
 
+    # Adding grid lines and setting axis labels and title
     ax.grid(linestyle = "--", linewidth = 0.5)
     ax.set(xlabel = xlabel, ylabel = ylabel, title = title)
 
@@ -30,7 +75,7 @@ def plot_metric_vs_metric(df, x, y, xlabel, ylabel, title, filename,
     ax.set_xlim(ax.get_xlim())
     ax.set_ylim(ax.get_ylim())
 
-    # Percentage guide lines
+    # Plotting percentage guide lines for each given guide percentage
     x_vals = np.linspace(df[x].min(), df[x].max(), 100)
     for idx, pct in enumerate(guide_pcts):
         pct_vals = (pct / 100) * x_vals
@@ -38,42 +83,66 @@ def plot_metric_vs_metric(df, x, y, xlabel, ylabel, title, filename,
         ax.plot(x_vals, pct_vals, linestyle, color = CB_COLORS["black"], label = f"{pct}{guide_label}")
         # TODO: different colors or weights for different lines: used alternating line styles for now
 
-    # Add horizontal lines for average and median for percentage plots
+    # For graphs with parser percentage on the y-axis, add horizontal lines
+    # for the average and median parser percentage
     if y == "parser_percentage":
         avg = df[y].mean()
         median = df[y].median()
         ax.axhline(y = avg, linestyle = "--", color = CB_COLORS["blue"], label = f"average ({avg:.3g})")
         ax.axhline(y = median, linestyle = "--", color = CB_COLORS["green"], label = f"median ({median:.3g})")
 
-    # Fits a power-law relationship and plot it
+    # Fiting a power-law relationship
     log_x = np.log(df[x])
     log_y = np.log(df[y])
     b, a = np.polyfit(log_x, log_y, 1)  # Fit log(y) = b * log(x) + a
     a_exp = np.exp(a)
 
+    # Plotting the best fit line
+    # TODO: remove duplicate or keep for clarity
     x_vals = np.linspace(df[x].min(), df[x].max(), 100)
     y_vals = a_exp * x_vals**b
     ax.plot(x_vals, y_vals, "--", color = CB_COLORS["red"],
             label=f"best-fit line $y = {a_exp:.3g}x^{{{b:.3g}}}$")
 
-    # Adding legend
+    # Adding a legend
     ax.legend(fontsize = "small")
 
-    # Saving the plot as a PDF
-    if log is True:
-        filename = filename.replace(".", "_log.")
+    # Creating the plots directory if it doesn't exist and saving the plot as a PDF
     os.makedirs(PLOTS_DIR, exist_ok = True)
     fig.savefig(os.path.join(PLOTS_DIR, filename), format = "pdf")
 
 
-def make_module_plots():
+def make_module_plots() -> None:
+    """
+    Make plots for the module statistics. Four plots are generated:
+    - Parser time vs Total time
+    - Percentage of time spent on parsing vs Total time
+    - Parser time vs Size
+    - Percentage of time spent on parsing vs Size
+
+    If an extension is not in the `PLOT_STYLES` dictionary, it is grouped under
+    the "other" category. The plots are saved as PDFs in the `plots` directory.
+
+    Args:
+        None
+
+    Returns:
+        None
+    """
+
+    # Read the module statistics file
     df = pd.read_csv(MODULE_STATS_FILE)
+
+    # Convert the times from milliseconds to seconds
     df["total_time"] = df["total_time"] / 1000
     df["parser_time"] = df["parser_time"] / 1000
 
-    # NOTE: comment/uncomment to show all / hide other extensions
+    # Group extensions not in PLOT_STYLES under "other".
+    # NOTE: comment to show all extensions with the default style
     df["extension"] = df["extension"].apply(lambda x: x if x in PLOT_STYLES else "other")
 
+    # Define the metrics to plot. Each metric contains the parameters to be used by the
+    # generic plotting function `plot_metric_vs_metric`.
     metrics = [
         ("total_time", "parser_time", "Total time (s)", "Parser time (s)",
          "module_parser_vs_total.pdf", [0.1, 1, 10], "%"),
@@ -85,6 +154,7 @@ def make_module_plots():
          "module_parser_pct_vs_size.pdf", [], ""),
     ]
 
+    # Generate the plots for each metric, both with and without log scale
     for x, y, xlabel, ylabel, filename, guide_pcts, guide_label in metrics:
         plot_metric_vs_metric(df, x, y, xlabel, ylabel, f"{ylabel} vs {xlabel} by modules",
                               filename, guide_pcts, guide_label)
@@ -92,11 +162,32 @@ def make_module_plots():
                               filename, guide_pcts, guide_label, log = False)
 
 
-def make_package_plots():
+def make_package_plots() -> None:
+    """
+    Make plots for the package statistics. Four plots are generated:
+    - Parser time vs Total time
+    - Percentage of time spent on parsing vs Total time
+    - Parser time vs Size
+    - Percentage of time spent on parsing vs Size
+
+    The plots are saved as PDFs in the `plots` directory.
+
+    Args:
+        None
+
+    Returns:
+        None
+    """
+
+    # Read the package statistics file
     df = pd.read_csv(PACKAGE_STATS_FILE)
+
+    # Convert the times from milliseconds to seconds
     df["total_time"] = df["total_time"] / 1000
     df["parser_time"] = df["parser_time"] / 1000
 
+    # Define the metrics to plot. Each metric contains the parameters to be used by the
+    # generic plotting function `plot_metric_vs_metric`.
     metrics = [
         ("total_time", "parser_time", "Total time (s)", "Parser time (s)",
          "package_parser_vs_total.pdf", [0.1, 1, 10], "%"),
@@ -108,6 +199,7 @@ def make_package_plots():
          "package_parser_pct_vs_size.pdf", [], ""),
     ]
 
+    # Generate the plots for each metric, both with and without log scale
     for x, y, xlabel, ylabel, filename, guide_pcts, guide_label in metrics:
         plot_metric_vs_metric(df, x, y, xlabel, ylabel, f"{ylabel} vs {xlabel} by packages",
                               filename, guide_pcts, guide_label, module = False)
@@ -115,12 +207,30 @@ def make_package_plots():
                               filename, guide_pcts, guide_label, log = False, module = False)
 
 
-def make_aggregated_plot():
+def make_aggregated_plot() -> None:
+    """
+    Makes an aggregated plot comparing the parser time against the total time
+    across all packages. The plot is saved as a PDF in the `plots` directory.
+
+    Args:
+        None
+
+    Returns:
+        None
+    """
+
+    # Read the package statistics file
     df = pd.read_csv(PACKAGE_STATS_FILE)
+
+    # Calculates the total time and parser time across all packages in seconds
+    # and the percentage of time spent on parsing
     total_time = df["total_time"].sum() / 1000
     parser_time = df["parser_time"].sum() / 1000
     parser_percentage = (parser_time / total_time) * 100
 
+    # Create a scatter plot comparing the parser time against the total time
+    # across all packages with a similar style as the package plots. Annotate
+    # the point with the total and parser time and the parser percentage.
     fig, ax = plt.subplots()
     ax.scatter(total_time, parser_time, **PACKAGE_STYLE)
     ax.grid(linestyle = "--", linewidth = 0.5)
@@ -129,8 +239,7 @@ def make_aggregated_plot():
     ax.annotate(f"{parser_time:.3g}s / {total_time:.3g}s\n({parser_percentage:.3g}%)",
                 (total_time, parser_time), 
                 textcoords = "offset points", xytext = (0, 10), ha = "center")
-    fig.savefig(os.path.join(PLOTS_DIR, "aggregated_parser_vs_total.pdf"), format = "pdf")
 
-
-if __name__ == "__main__":
-    pass
+    # Saves the plot as a PDF in the `plots` directory
+    filename = "aggregated_parser_vs_total.pdf"
+    fig.savefig(os.path.join(PLOTS_DIR, filename), format = "pdf")
--- timing_analysis.py
+++ timing_analysis.py
@@ -1,21 +1,52 @@
-import os
+"""
+This file contains the functions that deal with reading, cleaning and computing
+statistics from the timing data files. The statistics are computed for each module
+and package and are written to CSV files.
+"""
+
 import fnmatch
+import os
+from typing import Any, Dict, List, Tuple
+
 import json5
 import pandas as pd
 
-from constants import (
-    SOURCES_DIR, TIMINGS_DIR,
-    MODULE_STATS_FILE, PACKAGE_STATS_FILE,
-)
+from constants import (MODULE_STATS_FILE, PACKAGE_STATS_FILE, SOURCES_DIR,
+                       TIMINGS_DIR)
 
 
-def read_json_file(file):
+def read_json_file(file: str) -> Dict[str, Any]:
+    """
+    Read a JSON file using the json5 library. This is used to handle JSON files
+    with comments and trailing commas as the standard json library does not
+    support these features.
+
+    Args:
+        file: The path to the JSON file
+
+    Returns:
+        The contents of the JSON file as a dictionary
+    """
+
     with open(file) as f:
-        # json does not handle trailing commas in JSON files
         return json5.load(f)
 
 
-def read_and_clean_timings(filename):
+def read_and_clean_timings(filename: str) -> pd.DataFrame:
+    """
+    Read the timings from a JSON file and clean them by removing the systool and
+    hi/dyn_hi modules. Package names are added to the dataframe and the alloc
+    column is removed. The cleaned timings are then written to a CSV file and
+    returned as a dataframe.
+
+    Args:
+        filename: The name of the JSON file containing the timings
+
+    Returns:
+        A dataframe containing the cleaned timings data
+    """
+
+    # Read the timings from the JSON file
     file_path = os.path.join(TIMINGS_DIR, filename)
     data = read_json_file(file_path)
     timings_df = pd.DataFrame(data["data"])
@@ -23,42 +54,78 @@ def read_and_clean_timings(filename):
     # Remove systool and hi/dyn_hi modules
     timings_df = timings_df[~timings_df["module"].isin(["systool"]) & ~timings_df["module"].str.endswith(("hi", "dyn_hi"))]
 
+    # Add the package name to the dataframe and remove the alloc column
     package_name = filename.replace(".json", "")
     timings_df.insert(0, "package", package_name)
     timings_df.drop(["alloc"], axis = 1, inplace = True)
 
+    # Write the cleaned timings to a CSV file and return the dataframe
     output_file = os.path.join(TIMINGS_DIR, f"{package_name}_timings.csv")
     timings_df.to_csv(output_file, index = False)
     return timings_df
 
 
-def get_size_and_extension(package, module):
+def get_size_and_extension(package: str, module : str) -> Tuple[int, str]:
+    """
+    Get the size and extension of the file for a given module in a package. The
+    function searches for the file in the package's source directory and its
+    subdirectories. If the file is found, its size and extension are returned.
+
+    Args:
+        package: The name of the package
+        module: The name of the module
+
+    Returns:
+        A tuple containing the size of the file in bytes and its extension
+    """
+
+    # Get the possible source directories for the package
     package_path = os.path.join(SOURCES_DIR, package)
     src_dirs = ["", "src", "lib"]
 
+    # Split the module name into a list of path components and construct the search pattern
     module_path_list = module.split(".")
     module_path = os.path.join(*module_path_list[:-1]) if len(module_path_list) > 1 else ""
     search_pattern = f"{module_path_list[-1]}.*"
 
+    # Search for the file in the package's possible source directories
     for src_dir in src_dirs:
-        # Construct the search path for the module
+        # Construct the search path and check if it exists
         search_path = os.path.join(package_path, src_dir, module_path)
         if not os.path.isdir(search_path):
             continue
     
+        # Walk the directory and search for the file using the pattern
         for root, _dirs, files in os.walk(search_path):
             for name in files:
+                # Check if the file matches the search pattern
                 if fnmatch.fnmatch(name, search_pattern):
+                    # Get the size and extension of the file and return them
                     file_path = os.path.join(root, name)
                     size = os.path.getsize(file_path)
                     extension = os.path.splitext(file_path)[1]
                     return size, extension
 
+    # Print a message and return 0 and an empty string if the file was not found
     print(f"Could not find file for module {module} in package {package}.")
     return 0, ""
 
 
-def compute_statistics(df):
+def compute_statistics(df: pd.DataFrame) -> Tuple[pd.DataFrame, pd.DataFrame]:
+    """
+    Compute the statistics for a given dataframe corresponding to a package. The
+    function calculates the total time, parser time, parser percentage, size and
+    extension of the files for each module in the package. The statistics are then
+    aggregated for each module and package and returned as dataframes.
+
+    Args:
+        df: The dataframe containing the timings data for a package
+
+    Returns:
+        Two dataframes containing the module and package statistics respectively
+    """
+
+    # Check if the dataframe is empty
     if df.empty:
         raise ValueError("Dataframe is empty.")
 
@@ -66,16 +133,19 @@ def compute_statistics(df):
     modules = df["module"].unique()
     package_name = df["package"].iloc[0]
 
-    # Module stats
+    # Module statistics
     for module in modules:
+        # Get the size and extension of the file for the module
         module_size, module_extension = get_size_and_extension(package_name, module)
         if module_size == 0 and module_extension == "":
             continue
 
+        # Calculate the total time, parser time and parser percentage for the module
         total_time = df[df["module"] == module]["time"].sum()
         parser_time = df[(df["module"] == module) & (df["phase"] == "Parser")]["time"].sum()
         parser_percentage = (parser_time / total_time) * 100
         
+        # Create a dictionary with the module statistics and append it to the list
         stats = {
             "module": module,
             "total_time": total_time,
@@ -86,10 +156,13 @@ def compute_statistics(df):
         }
         module_stats.append(stats)
 
+    # Create a dataframe from the list of module statistics and add the package name
     module_stats_df = pd.DataFrame(module_stats)
     module_stats_df.insert(0, "package", package_name)
 
-    # Package stats
+    # Package statistics
+    # Calculate the total time, parser time, parser percentage, average parser percentage,
+    # geomean parser percentage and size for the package
     package_total_time = module_stats_df["total_time"].sum()
     package_parser_time = module_stats_df["parser_time"].sum()
     package_parser_percentage = (package_parser_time / package_total_time) * 100
@@ -97,6 +170,7 @@ def compute_statistics(df):
     geomean_parser_percentage = module_stats_df["parser_percentage"].prod() ** (1 / len(module_stats_df))
     package_size = module_stats_df["size"].sum()
     
+    # Create a dictionary with the package statistics and convert it to a dataframe
     package_stats = {
         "package": [package_name],
         "total_time": [package_total_time],
@@ -108,25 +182,37 @@ def compute_statistics(df):
     }
     package_stats_df = pd.DataFrame(package_stats)
 
+    # Return the dataframes containing the module and package statistics
     return module_stats_df, package_stats_df
 
 
-def calculate_statistics_for_packages(files):
+def calculate_statistics_for_packages(files: List[str]) -> None:
+    """
+    Calculate the statistics for the given list of timing data files. The function
+    reads the timings from the files, cleans them, computes the statistics for each
+    module and package, aggregates the statistics and writes them to CSV files.
+
+    Args:
+        files: A list of the names of the timing data files
+
+    Returns:
+        None
+    """
+
     all_module_stats = []
     all_package_stats = []
 
     for file in files:
+        # Read and clean the timings, compute the statistics and append them to the lists
         timings_df = read_and_clean_timings(file)
         module_stats_df, package_stats_df = compute_statistics(timings_df)
         all_module_stats.append(module_stats_df)
         all_package_stats.append(package_stats_df)
 
+    # Concatenates the list of module statistics dataframes and writes them to a CSV file
     all_module_stats_df = pd.concat(all_module_stats, ignore_index = True)
     all_module_stats_df.to_csv(MODULE_STATS_FILE, index = False)
 
+    # Concatenates the list of package statistics dataframes and writes them to a CSV file
     all_package_stats_df = pd.concat(all_package_stats, ignore_index = True)
     all_package_stats_df.to_csv(PACKAGE_STATS_FILE, index = False)
-
-
-if __name__ == "__main__":
-    pass
